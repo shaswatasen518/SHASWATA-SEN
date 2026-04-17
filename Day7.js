a978ef122984eb3e6e147a6630f5c826e987b92a
SHASWATA SEN <shaswatasen518@gmail.com>
2026-04-17 20:53:11 +0530

    Update Day7.js

--- Day7.js
+++ Day7.js
@@ -1,8 +1,115 @@
 // 1. Write a program to sort the characters of a string alphabetically. 
+
+
+// let str = "javascript";
+
+// let sorted = str.split("").sort().join("");
+
+// console.log(sorted); 
+
+
+
 // 2. Write a program that removes all duplicate characters from a string. 
+
+
+// let str = "programming";
+
+// let result = [...new Set(str)].join("");
+
+// console.log(result); 
+
+
+
+
 // 3. Create a program that checks whether two strings are anagrams of each other. 
+
+
+
+
+
+
+
+
+
+
+
+
+
 // 4. Write a program to mask the last 4 digits of a phone number (e.g., 9876543210 → ******3210). 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // 5. Write a program that finds the longest word in a sentence. 
+
+
+// let sentence = "JavaScript is very powerful language";
+
+// let words = sentence.split(" ");
+// let longest = words.reduce((a, b) => a.length > b.length ? a : b);
+
+// console.log(longest); 
+
+
+
+
+
+
 // 6. Write a program that trims leading and trailing spaces from a string and counts the actual length. 
+
+
+
+// let str = "   I am Shaswata   ";
+
+// let trimmed = str.trim();
+
+// console.log(trimmed);        
+// console.log(trimmed.length); 
+
+
+
 // 7. Write a program to convert a string into title case (every word starts with uppercase). 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // 8. Write a program that checks if a given string starts and ends with the same character.
+
+
+
+// let str = "level";
+
+// let result = str[0] === str[str.length - 1];
+
+// console.log(result); 
+
+
+
+

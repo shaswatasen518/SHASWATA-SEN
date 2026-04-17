91c6aaf947b7c3f081347f7d7eddff4a361c8b54
SHASWATA SEN <shaswatasen518@gmail.com>
2026-04-17 20:53:46 +0530

    Update Day4.js

--- Day4.js
+++ Day4.js
@@ -1,39 +1,39 @@
 // 1. Write a program that finds the largest of three numbers using only if-else (no Math.max). 
 
-let a = 10, b = 25, c = 15;
+// let a = 10, b = 25, c = 15;
 
-if (a >= b && a >= c) {
-    console.log("Largest:", a);
-} else if (b >= a && b >= c) {
-    console.log("Largest:", b);
-} else {
-    console.log("Largest:", c);
-}
+// if (a >= b && a >= c) {
+//     console.log("Largest:", a);
+// } else if (b >= a && b >= c) {
+//     console.log("Largest:", b);
+// } else {
+//     console.log("Largest:", c);
+// }
 
 
 
 // 2. Use switch to build a simple calculator that can perform addition, subtraction, multiplication, and division. 
 
 
-let num1 = 100, num2 = 65;
-let op = "-"; // +, -, *, /
+// let num1 = 100, num2 = 65;
+// let op = "-"; // +, -, *, /
 
-switch (op) {
-    case "+":
-        console.log(num1 + num2);
-        break;
-    case "-":
-        console.log(num1 - num2);
-        break;
-    case "*":
-        console.log(num1 * num2);
-        break;
-    case "/":
-        console.log(num2 !== 0 ? num1 / num2 : "Cannot divide by zero");
-        break;
-    default:
-        console.log("Invalid operator");
-}
+// switch (op) {
+//     case "+":
+//         console.log(num1 + num2);
+//         break;
+//     case "-":
+//         console.log(num1 - num2);
+//         break;
+//     case "*":
+//         console.log(num1 * num2);
+//         break;
+//     case "/":
+//         console.log(num2 !== 0 ? num1 / num2 : "Cannot divide by zero");
+//         break;
+//     default:
+//         console.log("Invalid operator");
+// }
 
 
 
@@ -41,11 +41,11 @@ switch (op) {
 // 3. Write a program to print all even numbers between 1 and 50 using a for loop. 
 
 
-for (let i = 1; i <= 50; i++) {
-    if (i % 2 === 0) {
-        console.log(i);
-    }
-}
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
 
 
 
@@ -65,24 +65,15 @@ for (let i = 1; i <= 50; i++) {
 
 
 
-
-
-
-
-
-
-
-
-
 // 6. Write a program that iterates through an array of numbers and prints only the odd numbers using a for...of loop. 
 
-let arr = [1, 2, 3, 4, 5, 6, 7];
+// let arr = [1, 2, 3, 4, 5, 6, 7];
 
-for (let num of arr) {
-    if (num % 2 !== 0) {
-        console.log(num);
-    }
-}
+// for (let num of arr) {
+//     if (num % 2 !== 0) {
+//         console.log(num);
+//     }
+// }
 
 
 
@@ -94,27 +85,27 @@ for (let num of arr) {
 // 7. Write a program to iterate through an object and print all key-value pairs using for...in. 
 
 
-let person = {
-    name: "John",
-    age: 25,
-    city: "Delhi"
-};
+// let person = {
+//     name: "John",
+//     age: 25,
+//     city: "Delhi"
+// };
 
-for (let key in person) {
-    console.log(key + ":", person[key]);
-}
+// for (let key in person) {
+//     console.log(key + ":", person[key]);
+// }
 
 
 
 // 8. Write a program that calculates the sum of digits of a number using a loop (e.g., 123 → 1+2+3 = 6).
 
-let num = 123;
-let sum = 0;
+// let num = 123;
+// let sum = 0;
 
-let str = num.toString();
+// let str = num.toString();
 
-for (let i = 0; i < str.length; i++) {
-    sum += Number(str[i]);
-}
+// for (let i = 0; i < str.length; i++) {
+//     sum += Number(str[i]);
+// }
 
-console.log("Sum of digits:", sum);
+// console.log("Sum of digits:", sum);

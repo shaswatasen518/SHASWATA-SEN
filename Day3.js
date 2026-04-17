3b1eaba1a919329f2f1c080c7fb2a3d90e9dc3c9
SHASWATA SEN <shaswatasen518@gmail.com>
2026-04-17 14:51:18 +0530

    Create Day3.js

---
+++ Day3.js
@@ -0,0 +1,96 @@
+
+
+//1. Write a program to check if a number is odd or even without using the modulus operator (%). 
+
+
+let num = 6;
+
+while (num > 1) {
+    num = num - 2;
+}
+
+if (num === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+
+
+
+// 2. Use the ternary operator to classify a student’s grade based on their score. 
+
+
+let score = 77;
+
+let grade = (score >= 90) ? "A" :
+            (score >= 75) ? "B" :
+            (score >= 50) ? "C" : "Fail";
+
+console.log("Grade:", grade);
+
+
+
+
+
+
+// 3. Demonstrate the difference between == and === by comparing variables of different data types. 
+
+
+let a = 5;
+let b = "5";
+
+console.log(a == b);   // true (only value compared)
+console.log(a === b);  // false (value + type compared)
+
+
+// 4. Write a program that swaps two numbers without using a third variable, only using arithmetic operators. 
+
+
+let x = 10, y = 20;
+
+x = x + y;
+y = x - y;
+x = x - y;
+
+console.log("x =", x, "y =", y);
+
+
+
+// 5. Use logical operators to check if a year is a leap year or not. 
+
+
+
+let year = 2025;
+
+if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    console.log("Leap Year");
+} else {
+    console.log("Not a Leap Year");
+} 
+
+
+
+// 6. Demonstrate the use of the nullish coalescing operator (??) with multiple fallback values. 
+
+
+
+
+
+
+
+
+
+
+// 7. Write a program that uses bitwise operators to check if a given number is a power of 2. 
+
+
+
+
+
+
+
+// 8. Write a program that calculates the area of a rectangle only if both length and breadth are greater than 0, using short-circuit evaluation (&&). 
+
+
+
+

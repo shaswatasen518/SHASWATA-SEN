46d9537bfd1502c32f6467fad2b3b20c9e4e946b
SHASWATA SEN <shaswatasen518@gmail.com>
2026-04-17 14:53:52 +0530

    Update Day10.js

--- Day10.js
+++ Day10.js
@@ -3,17 +3,17 @@
 // 1. Write a program to create an array of objects representing students with name and marks, and print only the names of students who scored more than 50. 
 
 
-// let students = [
-//   { name: "Amit", marks: 60 },
-//   { name: "Rahul", marks: 45 },
-//   { name: "Sita", marks: 75 }
-// ];
+let students = [
+  { name: "Amit", marks: 60 },
+  { name: "Rahul", marks: 45 },
+  { name: "Sita", marks: 75 }
+];
 
-// let result = students
-//   .filter(student => student.marks > 50)
-//   .map(student => student.name);
+let result = students
+  .filter(student => student.marks > 50)
+  .map(student => student.name);
 
-// console.log(result);
+console.log(result);
 
 
 
@@ -22,36 +22,36 @@
 
 
 
-// let people = [
-//   { name: "A", age: 30 },
-//   { name: "B", age: 20 },
-//   { name: "C", age: 25 }
-// ];
+let people = [
+  { name: "A", age: 30 },
+  { name: "B", age: 20 },
+  { name: "C", age: 25 }
+];
 
-// people.sort((a, b) => a.age - b.age);
+people.sort((a, b) => a.age - b.age);
 
-// console.log(people);
+console.log(people);
 
 
 // 3. Write a program to find the student with the highest marks from an array of student objects. 
 
 
 
-// let students = [
-//   { name: "Sumit", marks: 60 },
-//   { name: "Bakul", marks: 85 },
-//   { name: "Gita", marks: 75 }
-// ];
+let students = [
+  { name: "Sumit", marks: 60 },
+  { name: "Bakul", marks: 85 },
+  { name: "Gita", marks: 75 }
+];
 
-// let topStudent = students[0];
+let topStudent = students[0];
 
-// for (let i = 1; i < students.length; i++) {
-//   if (students[i].marks > topStudent.marks) {
-//     topStudent = students[i];
-//   }
-// }
+for (let i = 1; i < students.length; i++) {
+  if (students[i].marks > topStudent.marks) {
+    topStudent = students[i];
+  }
+}
 
-// console.log(topStudent);
+console.log(topStudent);
 
 
 
@@ -69,21 +69,21 @@
 
 
 
-// let students = [
-//   { name: "Pradip", marks: 60 },
-//   { name: "Rabul", marks: 80 },
-//   { name: "Mita", marks: 100 }
-// ];
+let students = [
+  { name: "Pradip", marks: 60 },
+  { name: "Rabul", marks: 80 },
+  { name: "Mita", marks: 100 }
+];
 
-// let total = 0;
+let total = 0;
 
-// for (let i = 0; i < students.length; i++) {
-//   total += students[i].marks;
-// }
+for (let i = 0; i < students.length; i++) {
+  total += students[i].marks;
+}
 
-// let avg = total / students.length;
+let avg = total / students.length;
 
-// console.log(avg);
+console.log(avg);
 
 
 
@@ -92,15 +92,15 @@
 
 
 
-// let employees = [
-//   { name: "A", salary: 20000 },
-//   { name: "B", salary: 50000 },
-//   { name: "C", salary: 30000 }
-// ];
+let employees = [
+  { name: "A", salary: 20000 },
+  { name: "B", salary: 50000 },
+  { name: "C", salary: 30000 }
+];
 
-// let filtered = employees.filter(emp => emp.salary >= 30000);
+let filtered = employees.filter(emp => emp.salary >= 30000);
 
-// console.log(filtered);
+console.log(filtered);
 
 
 
@@ -123,15 +123,15 @@
 // 8. Write a program that checks if all products in an array of objects are in stock (inStock = true).
 
 
-// let products = [
-//   { name: "Pen", inStock: true },
-//   { name: "Book", inStock: true },
-//   { name: "Bag", inStock: false }
-// ];
+let products = [
+  { name: "Pen", inStock: true },
+  { name: "Book", inStock: true },
+  { name: "Bag", inStock: false }
+];
 
-// let allAvailable = products.reduce((acc, p) => acc && p.inStock, true);
+let allAvailable = products.reduce((acc, p) => acc && p.inStock, true);
 
-// console.log(allAvailable);
+console.log(allAvailable);
 
 
 

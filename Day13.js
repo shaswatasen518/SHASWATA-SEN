630459cc45ee8a5b0b27e5f7a6c83d563ab90b68
SHASWATA SEN <shaswatasen518@gmail.com>
2026-04-17 14:54:21 +0530

    Update Day13.js

--- Day13.js
+++ Day13.js
@@ -1,31 +1,31 @@
 // 1. Write a function that takes an unknown number of arguments using the rest parameter and returns their sum. 
 
 
-// function sum(...numbers) {
-//     return numbers.reduce((total, num) => total + num, 0);
-// }
+function sum(...numbers) {
+    return numbers.reduce((total, num) => total + num, 0);
+}
 
-// console.log(sum(1, 2, 3, 4)); 
+console.log(sum(1, 2, 3, 4)); 
 
 
 // 2. Write a function that finds the maximum number from an array using the spread operator. 
 
 
-// function findMax(arr) {
-//     return Math.max(...arr);
-// }
+function findMax(arr) {
+    return Math.max(...arr);
+}
 
-// console.log(findMax([10, 20, 5, 40])); 
+console.log(findMax([10, 20, 5, 40])); 
 
 
 // 3. Write a program that merges two arrays using the spread operator. 
 
-// let arr1 = [1, 2, 3];
-// let arr2 = [4, 5, 6];
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
 
-// let merged = [...arr1, ...arr2];
+let merged = [...arr1, ...arr2];
 
-// console.log(merged); 
+console.log(merged); 
 
 
 // 4. Write a program that copies an object using the spread operator (deep vs shallow copy case). 
@@ -38,28 +38,28 @@
 // 5. Write a function with rest parameters that calculates the average of any number of inputs. 
 
 
-// const average = (...nums) => {
-//     let total = 0;
+const average = (...nums) => {
+    let total = 0;
 
-//     nums.forEach(n => total += n);
+    nums.forEach(n => total += n);
 
-//     return total / nums.length;
-// };
+    return total / nums.length;
+};
 
-// console.log(average(10, 20, 30, 40));
+console.log(average(10, 20, 30, 40));
 
 
 
 // 6. Write a program that combines multiple objects into one using the spread operator. 
 
 
-// let obj1 = { a: 1 };
-// let obj2 = { b: 2 };
-// let obj3 = { c: 3 };
+let obj1 = { a: 1 };
+let obj2 = { b: 2 };
+let obj3 = { c: 3 };
 
-// let combined = { ...obj1, ...obj2, ...obj3 };
+let combined = { ...obj1, ...obj2, ...obj3 };
 
-// console.log(combined); 
+console.log(combined); 
 
 
 
